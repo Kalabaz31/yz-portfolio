01b9ce6e95d636296696bb5c5d5cf76fdfd661b9
kalabaz31 <zahzouhy@gmail.com>
2022-09-17 19:42:02 +0100

    about section

--- components/About.jsx
+++ components/About.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import SectionTitle from './SectionTitle'
 
 const About = () => {
     return (
-        <div className='min-h-full bg-black'>About</div>
+        <div className='min-h-full py-4 flex flex-col  items-center'>
+            <SectionTitle title="ABOUT ME" backgroundTitle="ABOUT" />
+
+            <div className='bg-green-400/10 p-2 relative overflow-hidden rounded w-[100%] max-w-[700px]'>
+                <div className='absolute top-0 left-0 w-full h-[8px] animate-translate_x_100'>
+                    <span className='absolute top-0 left-0  w-[35px] h-[8px]  rounded bg-gradient-to-r from-transparent to-green-400'></span>
+                </div>
+
+                <div className='absolute bottom-0 left-0 w-full h-[8px] animate-translate_x_100_reverse'>
+                    <span className='absolute top-0 left-0 w-[35px] h-[8px]  rounded bg-gradient-to-r from-green-400 to-transparent'></span>
+                </div>
+
+                <div className='absolute top-0 right-0 h-full w-[8px] animate-translate_y_100'>
+                    <span className='absolute top-0 left-0 h-[35px] w-[8px]  rounded bg-gradient-to-t from-green-400 to-transparent'></span>
+                </div>
+
+                <div className='absolute top-0 left-0 h-full w-[8px] animate-translate_y_100_reverse'>
+                    <span className='absolute bottom-0 left-0 h-[35px] w-[8px]  rounded bg-gradient-to-b from-green-400 to-transparent'></span>
+                </div>
+
+                <img src="https://tf-react-bieber.vercel.app/images/about-image.jpg" alt="" 
+                className='object-cover'
+                />
+            </div>
+        </div>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,36 @@ module.exports = {
     extend: {
       animation: {
         'spin-2s': 'spin 2s linear infinite',
+        translateXAnimation: 'translateXAnimation 2s ease-in-out infinite',
+        translate_x_100: 'translate_x_100 4s ease-in-out infinite',
+        translate_x_100_reverse: 'translate_x_100_reverse 4s ease-in-out infinite',
+        translate_y_100: 'translate_y_100 4s ease-in-out infinite',
+        translate_y_100_reverse: 'translate_y_100_reverse 4s ease-in-out infinite',
+
       }
+      , keyframes: {
+        translateXAnimation: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(150px)' },
+        },
+        translate_x_100: {
+          '0%': { transform: 'translateX(-35px)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+        translate_x_100_reverse: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-35px)' },
+        },
+        translate_y_100: {
+          '0%': { transform: 'translateY(-35px)' },
+          '100%': { transform: 'translateY(100%)' },
+        },
+        translate_y_100_reverse: {
+          '0%': { transform: 'translateY(35px)' },
+          '100%': { transform: 'translateY(-100%)' },
+        }
+      }
+
 
     },
   },
